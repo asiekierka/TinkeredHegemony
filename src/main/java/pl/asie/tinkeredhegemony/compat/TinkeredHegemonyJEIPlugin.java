df3cb25e861c658d973c588bcd7f70eb79f55aa9
Adrian Siekierka <kontakt@asie.pl>
2018-06-25 12:59:29 +0200

    update headers

--- src/main/java/pl/asie/tinkeredhegemony/compat/TinkeredHegemonyJEIPlugin.java
+++ src/main/java/pl/asie/tinkeredhegemony/compat/TinkeredHegemonyJEIPlugin.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2015, 2016, 2017, 2018 Adrian Siekierka
+ *
+ * This file is part of Tinkered Hegemony.
+ *
+ * Tinkered Hegemony is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tinkered Hegemony is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Tinkered Hegemony.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package pl.asie.tinkeredhegemony.compat;
 
 import mezz.jei.api.IModPlugin;
